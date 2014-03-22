7bfd45d9faa7fea8ff351c0603aaf3a01c916b62
Manjunath Kaliyur <manjunatha.g.kaliyur@kp.org>
2014-03-21 20:00:28 -0700

    update to add nginx dependency in meta

--- bizzns/base/metadata.rb
+++ bizzns/base/metadata.rb
@@ -1,7 +1,13 @@
+##metadata.rb
+
 name             'base'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Manjunath Kaliyur'
+maintainer_email 'mkaliyur@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures base'
+description      'Installs/Configures bizzns cookbook - defines dependencies'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+ 
+
+
+depends 'nginx',             '~> 2.4.4' 
---
+++ bizzns/base/recipes/base.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: bizzns
+# Recipe:: base
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
