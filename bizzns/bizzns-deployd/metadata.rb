d676dcb4dfd8cfda885ca9e7b62802591efb4e6f
Manjunath Kaliyur <manjunatha.g.kaliyur@kp.org>
2014-03-28 21:56:45 -0700

    moved nodejs to nodejs cookbook, added deployd cookbook

---
+++ bizzns/bizzns-deployd/metadata.rb
@@ -0,0 +1,18 @@
+##metadata.rb
+
+name             'base'
+maintainer       'Manjunath Kaliyur'
+maintainer_email 'mkaliyur@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures bizzns cookbook - defines dependencies'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+
+depends 'chef-deployd'
+
+
+
+ 
+
+
---
+++ bizzns/bizzns-deployd/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2014, Bizzns, Inc
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "deployd"
+
+
+
+
+
+
+
+
