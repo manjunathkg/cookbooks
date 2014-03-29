00b8ec964eebbba583a9e87d7140a2b5f5d11ebf
Manjunath Kaliyur <manjunatha.g.kaliyur@kp.org>
2014-03-28 22:41:04 -0700

    copied chef-deployed recipe to bizzns to remove hard dependency on mongo etc

--- bizzns/bizzns-deployd/recipes/default.rb
+++ bizzns/bizzns-deployd/recipes/default.rb
@@ -1,18 +1,41 @@
 #
-# Cookbook Name:: base
+# Cookbook Name:: deployd
 # Recipe:: default
 #
-# Copyright 2014, Bizzns, Inc
+# Copyright 2013, Adam Krone
+# Authors:
+# 		Adam Krone <krone.adam@gmail.com>
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-include_recipe "deployd"
+# Install dependecies 
+include_recipe "deployd::user"
 
+# Install deployd with npm
+execute "install deployd with npm" do
+	command "sudo npm install -g deployd"
+	action :run
+end
 
+# Change permissions for dpd
+execute "chown dpd" do
+	command "sudo chown #{node['deployd']['user']}:#{node['deployd']['group']} /usr/local/bin/dpd"
+	action :run
+end
 
-
-
-
-
-
+# Change permissions for deployd lib
+execute "chown deployd lib" do
+	command "sudo chown -R #{node['deployd']['user']}: /usr/local/lib/node_modules/deployd"
+	action :run
+end
